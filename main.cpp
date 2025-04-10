edea3c46159a5ae93461e0ecf43d7a1277d072c9
Krelan <128473555+Krelan@users.noreply.github.com>
2025-04-11 04:19:21 +0700

    Смена способа вывода
    Вывод текста посредством Qt Widgets App

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
-#include "mainwindow.h"
 
 #include <QApplication>
 #include <QLabel>
 
 int main(int argc, char *argv[])
 {
-    QApplication win(argc, argv);
-    MainWindow w;
-    w.show();
-    return win.exec();
+    QApplication app(argc, argv);
+        QLabel label("Hello, another World");
+            label.setWindowTitle("Qt Widgets App");
+            label.resize(300, 100);
+            label.show();
+            label.setAlignment(Qt::AlignCenter);
+            label.setStyleSheet("font-size: 16pt;");
+      return app.exec();
 }
